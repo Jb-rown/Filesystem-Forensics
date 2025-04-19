a76d0d495a27b2573490fc7aef05033ee08d7e38
Jb-rown <brownjohn9870@gmail.com>
2025-04-19 17:28:04 +0300

    Add utils.h and utils.c for utility functions

---
+++ utils.c
@@ -0,0 +1,10 @@
+#include "utils.h"
+#include <stdio.h>
+#include <unistd.h>
+
+// Function to check the integrity of the file system
+void check_fs_integrity(const char *device_path) {
+    printf("[+] Checking file system integrity: %s\n", device_path);
+    // Future updates to run relevant file system integrity checks.
+}
+
---
+++ utils.h
@@ -0,0 +1,6 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+void check_fs_integrity(const char *device_path);
+
+#endif
